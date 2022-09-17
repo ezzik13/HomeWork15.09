d0d95df85447eba88659f456fe515282ad81569f
Nikolai <ezzik13@mail.ru>
2022-09-17 23:47:08 +0500

    ready EX2

---
+++ EX2/Program.cs
@@ -0,0 +1,22 @@
+﻿// найти максимум из 5 чисел введенных пользователем
+
+using static System.Console;
+
+Clear();
+WriteLine("Введите 2 чисела: ");
+int num1 = int.Parse(ReadLine()!);
+int num2 = int.Parse(ReadLine()!);
+int max=0;
+int min=0;
+
+if(num2 > num1)
+{
+     max = num2;
+     min = num1;
+}
+else
+{
+     max = num1;
+     min = num2;
+}
+WriteLine($"{max}>{min}");
