be0a9b309d9bb62f23f027648a6ea8d794a1e9f1
Nikolai <ezzik13@mail.ru>
2022-09-18 00:00:10 +0500

    ready EX4

--- EX4/Program.cs
+++ EX4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// найти максимум из 3 чисел введенных пользователем
+
+using static System.Console;
+
+Clear();
+WriteLine("Введите 3 числа: ");
+int num1 = int.Parse(ReadLine()!);
+int num2 = int.Parse(ReadLine()!);
+int num3 = int.Parse(ReadLine()!);
+int max = num1;
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+WriteLine($"максимум из :{num1}, {num2}, {num3} = {max}");
