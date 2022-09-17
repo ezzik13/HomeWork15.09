2f3e376eb7f0931ee8cf344126fbf030f1a3e731
Nikolai <ezzik13@mail.ru>
2022-09-18 00:34:33 +0500

    ready EX8

--- EX8/Program.cs
+++ EX8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести все четные числа  от 1 до числа введенного пользователем?
+
+using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+int num1 = 1;
+int num2 = int.Parse(ReadLine()!);
+while(num1<=num2)
+{
+    if(num1 % 2==0)
+    Write ($"{num1}, ");
+    num1++;}
